f8575b1807104b7d8de231f78bb5f38f0d8cd241
MwirigiMuriithi <learnerviner@gmail.com>
2025-03-07 01:39:56 +0300

    configuring tailwind css: update Home.tsx with Tailwind styling

--- resources/js/Pages/Home.tsx
+++ resources/js/Pages/Home.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
 const Home = () => {
-    return (
-        <div>
-            <h1>Welcome to your Inertia.js and React setup!</h1>
-        </div>
-    );
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="text-center p-6">
+        <h1 className="text-4xl font-bold text-blue-600">
+          Welcome to your Inertia.js and React setup!
+        </h1>
+        <p className="mt-4 text-lg text-gray-700">
+          This is your home page styled with Tailwind CSS.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
