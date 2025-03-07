764a24f857978dd73c13526c00e1e2eca0dc64b1
MwirigiMuriithi <learnerviner@gmail.com>
2025-03-07 10:40:59 +0300

    feat: add AssignmentAnswer model

---
+++ app/Models/AssignmentAnswer.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AssignmentAnswer extends Model
+{
+    /** @use HasFactory<\Database\Factories\AssignmentAnswerFactory> */
+    use HasFactory;
+}
