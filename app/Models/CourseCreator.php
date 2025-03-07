cf4693bef81e21351e0a4da054203fdd93cbc966
MwirigiMuriithi <learnerviner@gmail.com>
2025-03-07 10:40:59 +0300

    feat: add CourseCreator model

---
+++ app/Models/CourseCreator.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CourseCreator extends Model
+{
+    /** @use HasFactory<\Database\Factories\CourseCreatorFactory> */
+    use HasFactory;
+}
